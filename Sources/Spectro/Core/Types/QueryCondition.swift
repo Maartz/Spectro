d678c43820c50aec9ed76ff905c77297687e88d6
Maartz <maartz@protonmail.com>
2024-11-15 22:56:31 +0100

    update query conditions to handle or and and

--- Sources/Spectro/Core/Types/QueryCondition.swift
+++ Sources/Spectro/Core/Types/QueryCondition.swift
@@ -5,8 +5,21 @@
 //  Created by William MARTIN on 11/15/24.
 //
 
-public struct QueryCondition {
-   let field: String
-   let op: String
-   let value: ConditionValue
+public struct QueryCondition: Condition {
+    let field: String
+    let op: String
+    let value: ConditionValue
+
+    // Add operators
+    static func && (lhs: QueryCondition, rhs: QueryCondition)
+        -> CompositeCondition
+    {
+        CompositeCondition(type: .and, conditions: [lhs, rhs])
+    }
+
+    static func || (lhs: QueryCondition, rhs: QueryCondition)
+        -> CompositeCondition
+    {
+        CompositeCondition(type: .or, conditions: [lhs, rhs])
+    }
 }
