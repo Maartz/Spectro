ee4463335e72a23167069f71ece51032f614343c
Maartz <maartz@protonmail.com>
2024-11-15 10:19:59 +0100

    add SQLKit in case of need

--- Package.swift
+++ Package.swift
@@ -12,11 +12,13 @@ let package = Package(
         .library(name: "Spectro", targets: ["Spectro"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/postgres-kit.git", from: "2.0.0")
+        .package(url: "https://github.com/vapor/postgres-kit.git", from: "2.0.0"),
+        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.32.0")
     ],
     targets: [
         .target(name: "Spectro", dependencies: [
-            .product(name: "PostgresKit", package: "postgres-kit")
+            .product(name: "PostgresKit", package: "postgres-kit"),
+            .product(name: "SQLKit", package: "sql-kit")
         ]),
         .testTarget(name: "SpectroTests", dependencies: ["Spectro"])
     ]
