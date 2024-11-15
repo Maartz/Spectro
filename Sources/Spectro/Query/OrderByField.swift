146145809c6257a060894235ddeab1f06dcbb02c
Maartz <maartz@protonmail.com>
2024-11-15 23:16:58 +0100

    struct that helds on which field and which direction we order

---
+++ Sources/Spectro/Query/OrderByField.swift
@@ -0,0 +1,11 @@
+//
+//  OrderByField.swift
+//  Spectro
+//
+//  Created by William MARTIN on 11/15/24.
+//
+
+public struct OrderByField: Sendable {
+    let field: String
+    let direction: OrderDirection
+}
