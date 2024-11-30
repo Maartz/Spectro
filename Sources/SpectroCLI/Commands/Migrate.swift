1ff269216f0257e1c42291efce8fcb4505e6bf60
Maartz <maartz@protonmail.com>
2024-11-30 16:32:20 +0100

    make migrate and rollback use the config loader

--- Sources/SpectroCLI/Commands/Migrate.swift
+++ Sources/SpectroCLI/Commands/Migrate.swift
@@ -14,16 +14,28 @@ struct Migrate: AsyncParsableCommand {
     )
 
     @Option(name: .long, help: "Database Username")
-    var username: String = "postgres"
+    var username: String?
 
     @Option(name: .long, help: "Database Password")
-    var password: String = "postgres"
+    var password: String?
 
     @Option(name: .long, help: "Database Name")
-    var database: String = "spectro_test"
+    var database: String?
 
     func run() async throws {
-        let spectro = try Spectro(username: username, password: password, database: database)
+        try ConfigurationManager.shared.loadEnvFile()
+        var overrides: [String: String] = [:]
+        if let username = username { overrides["username"] = username }
+        if let password = password { overrides["password"] = password }
+        if let database = database { overrides["database"] = database }
+
+        let config = ConfigurationManager.shared.getDatabaseConfig(overrides: overrides)
+        let spectro = try Spectro(
+            hostname: config.hostname,
+            port: config.port,
+            username: config.username,
+            password: config.password,
+            database: config.database)
 
         defer {
             spectro.shutdown()
